124553f3cec5981a35f33cd33ccbde0451022a46
hello17237 <zhaoregis@gmail.com>
2022-01-03 01:10:46 -0500

    minor change to photo paragraph

--- src/components/Photography/PhotoParagraph.js
+++ src/components/Photography/PhotoParagraph.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function PhotoParagraph() {
     return(
-        <p className="PhotoParagraph">I really enjoy taking pictures in my free time but by no means am I a professional. I own a pretty old camera (Nikon D40) and I usually just use a 50mm f1.8 lens with it. I know, it's basic, but it's cheap! I'm also too lazy to learn Photoshop so I usually just edit using the pre-installed Photos app on my laptop LOL (like I said, I'm nowhere near pro). Regardless, here are some of my personal favourites. Enjoy!</p>
+        <p className="PhotoParagraph">I really enjoy taking pictures in my free time. I own an old Nikon D40 and I usually just use a 50mm f1.8 lens with it. I know, basic, but it's cheap! Learning Photoshop is also too big of a time commitment for me (and it costs money) so I typically edit using the pre-installed Photos app on my laptop LOL. As you can probably tell, I'm not exactly a pro. Regardless, here are some of my personal favourites. Enjoy!</p>
     )
 }
 
